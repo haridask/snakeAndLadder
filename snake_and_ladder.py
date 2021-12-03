2c6e15d251d8380cdfbc3d9900c2f71dc9b53874
haridask <haridas124@gmail.com>
2021-12-03 20:27:20 +0530

    Add playing logic

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -1,21 +1,44 @@
 import logging
-from os import name
 import sys
+import random
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='SnakeAndLadder : %(message)s')
 
 class SnakeAndLadder:
 
-    def __init__(self, board_size: int=100) -> None:
+    def __init__(self, board_size: int=100, config: dict={}) -> None:
         self.board_size = board_size
+        self.config = config
+        self.validateConfig()
         self.log("Initializing snake and ladder in a board of size of {}".format(board_size))
 
+    def validateConfig(self):
+        for key, value in self.config.items():
+            if not 0 < value + key <= 100:
+                logging.error("InvalidConfiguration : {} steps at position {}".format(value, key))
+                logging.error("Exiting game")
+                sys.exit()
+
     def play(self, turns):
-        self.log("Starting game with {} turns".format(turns))
-        pass
+        self.log("Starting game with {} turns at position 1".format(turns))
+        position = 1
+        while turns > 0:
+            diceRoll = self.getDiceRoll()
+            
+            if position == 100:
+                self.log("Player has won with {} turns left".format(turns))
+                sys.exit()
+            elif position + diceRoll > 100:
+                self.log("Player cannot move ahead with die roll {} from position {}".format(diceRoll, position))
+            else:
+                position += diceRoll
+                self.log("Die rolled {} and player moved to position {}".format(diceRoll, position))
+            turns -= 1
+
+        self.log("Uh-oh! Player lose :(")
 
     def getDiceRoll(self):
-        pass
+        return random.randrange(1, 7)
 
     def log(self, message: str) -> None:
         logging.info(message)
--- test_snake_and_ladder.py
+++ test_snake_and_ladder.py
@@ -2,7 +2,10 @@
 from snake_and_ladder import SnakeAndLadder
 
 def test():
-    game = SnakeAndLadder()
+    # key = position at which snake/ladder starts
+    # value = number of places ahead/back it takes indicated by +ve/-ve numbers
+    config = { 10 : 5, 17 : 9, 28 : 11, 33 : -8, 40 : 3, 51 : -10, 62 : 9, 73 : -15, 82: 6,  95: -20 }
+    game = SnakeAndLadder(config=config)
     game.play(10)
 
 
